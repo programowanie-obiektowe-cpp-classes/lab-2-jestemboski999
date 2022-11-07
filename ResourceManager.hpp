2337b11cc83f1f9bb569a13f46c2ca0771dcbe0e
jestemboski999 <jestemboski999@gmail.com>
2022-11-07 12:16:57 +0100

    FW1

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,8 +1,43 @@
 #pragma once
-
 #include "Resource.hpp"
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+public:
+    ResourceManager()       //KONSTRUKTOR DOMYŚLNY
+    {
+        res = new Resource{};
+    }
+
+    ResourceManager(const ResourceManager& a)   //KONSTRUKTOR KOPIUJACY
+    {
+        res = new Resource{};
+        *res = *a.res;
+    }
+    ~ResourceManager() {    //DESTRUKTOR
+        delete res;
+    }
+
+    double get() { return res->get(); }
+
+    ResourceManager& operator=(const ResourceManager& b)
+    {
+        delete this->res;
+        res = new Resource{};
+        *res = *b.res;
+        return *this;
+    }
+
+private:
+    Resource* res;
 };
+
+//   ResourceManager& operator=( ResourceManager& w1,const ResourceManager& w2)
+//     {
+//         delete w1.res;
+//         w1.res = new Resource{};
+//         *w1.res = *w2.res;
+//         return w1;
+//     }
+
+//     w1=w2;
